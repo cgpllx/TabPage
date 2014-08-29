1da5fe878924283fba439108ebeb846efe074ba7
cgpllx <cgpllx1@qq.com>
2014-08-29 21:04:02 +0800

    Signed-off-by: cgpllx <cgpllx1@qq.com>

---
+++ src/com/kubeiwu/commontool/view/tabpagerindicator/IconPagerAdapter.java
@@ -0,0 +1,11 @@
+package com.kubeiwu.commontool.view.tabpagerindicator;
+
+public interface IconPagerAdapter {
+    /**
+     * Get icon representing the page at {@code index} in the adapter.
+     */
+    int getIconResId(int index);
+
+    // From PagerAdapter
+    int getCount();
+}
